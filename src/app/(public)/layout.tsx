78d9e7affc379c3a4a0a4bb1605083c15c1b0cd9
Verttb <alyssonoliveirai456@gmail.com>
2025-10-29 18:58:53 -0300

    feat: user context

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,5 +1,7 @@
 
 import Menu from "@/components/MenuLateral/Menu";
+import UserProvider from "@/core/providers/UserProvider";
+import { User } from "lucide-react";
 
 
 export default function RootLayout({
@@ -8,20 +10,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-   <>
-    <div
-      className="w-[400px] flex flex-col items-center"
-      style={{
-        boxShadow: "2px 0 3px rgba(0, 0, 0, .4)"
-      }}
-    >
-      <img src="/logo.png" className="w-[65%]" alt="Logo" />
-      <Menu />
-    </div>
+    <UserProvider>
+     <>
+      <div
+        className="w-[400px] flex flex-col items-center"
+        style={{
+          boxShadow: "2px 0 3px rgba(0, 0, 0, .4)"
+        }}
+      >
+        <img src="/logo.png" className="w-[65%]" alt="Logo" />
+        <Menu />
+      </div>
 
-    <div className="p-4 w-full">
-      {children}
-    </div>
+      <div className="p-4 w-full">
+        {children}
+      </div>
    </>
+    </UserProvider>
   );
 }
