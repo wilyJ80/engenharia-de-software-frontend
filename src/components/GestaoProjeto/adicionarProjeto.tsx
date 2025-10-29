b33de173e46e67eda3b0f0a7502b14527cd01de1
Verttb <alyssonoliveirai456@gmail.com>
2025-10-29 11:43:23 -0300

    refactor: adiconando funcoes no card

--- src/components/GestaoProjeto/adicionarProjeto.tsx
+++ src/components/GestaoProjeto/adicionarProjeto.tsx
@@ -11,9 +11,11 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+
 import { Input } from "../ui/input";
 import { Label } from "@radix-ui/react-label";
 import { Textarea } from "../ui/textarea";
+import { Projeto } from "@/core/interface/Projeto";
 
 const participantesMock = [
     "Alice",
@@ -22,43 +24,81 @@ const participantesMock = [
     "David",
     "Eve"
 ];  
+interface AdicionarProjetoProps {
+    onAddProjeto: (novoProjeto: Projeto) => void;
+}
 
-export const AdicionarProjeto = () => {
+export const AdicionarProjeto = ({ onAddProjeto }: AdicionarProjetoProps) => {
     const [open, setOpen] = useState(false);
+    const [nome, setNome] = useState("");
+    const [descricao, setDescricao] = useState("");
     const [ participantes, setParticipantes ] = useState<string[]>([]);
+
+    const onNomeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNome(e.target.value);
+    }
+    const onDescricaoChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setDescricao(e.target.value);
+    }
 
     return (
         <>
             <Button
-                  className="bg-azul-escuro rounded-md self-end"
+                  className="bg-azul-escuro hover:bg-azul-claro rounded-md place-self-end text-white cursor-pointer"
                   onClick={() => setOpen(true)}>
                 + Projeto
             </Button>
+            { open && (
             <Dialog  open={open} onOpenChange={setOpen}>
                <DialogContent className="bg-azul-escuro text-white border-none rounded-md">
                 <DialogTitle></DialogTitle>
                 <div className="flex flex-col gap-4">
 
                     <div className="flex gap-1 items-center">
-                        <Label className="mb-2">Nome:</Label>
-                        <Input type="text" placeholder="Nome do Projeto" className="bg-azul-escuro placeholder-red text-white"/>
+                        <Label className="">Nome:</Label>
+                        <Input 
+                             onChange={onNomeChange}
+                             type="text"
+                             placeholder="Nome do Projeto"
+                             className="bg-azul-escuro placeholder-red text-white"
+                             value={nome}/>
                     </div>
 
                     <div className="flex gap-1 items-center">
-                        <Label className="w-20">Descritivo:</Label>
-                        <Textarea placeholder="Descrição do Projeto"  className="bg-azul-escuro placeholder-white text-white mt-2"/>
+                        <Label className="">Descritivo:</Label>
+                        <Textarea 
+                                onChange={onDescricaoChange}
+                                placeholder="Descrição do Projeto"
+                                className="bg-azul-escuro placeholder-white text-white mt-2"
+                                value={descricao}/>
                     </div>
                 <div>
-
+                <div className="flex gap-2 items-center">
+                    <Label className="">Participantes:</Label>
+                     <select className="border-2 rounded-md py-1 w-full"> 
+                        {participantesMock.map((participante) => (
+                            <option className="text-black" key={participante} value={participante}>
+                                {participante}
+                            </option>
+                        ))}
+                    </select>
+                    <Button className="bg-azul-claro px-2  cursor-pointer hover:bg-azul-claro/50">Adicionar</Button>
+                    </div>
                 </div>
                 </div>
                 <DialogFooter>
-                    <Button className="bg-azul-escuro rounded-md border-2 px-10 border-black">
+                    <Button 
+                            onClick={() => {
+                                onAddProjeto({ id: Math.random().toString(36).substr(2, 9), nome, descricao });
+                                setOpen(false);
+                            }}
+                            className="bg-azul-escuro hover:bg-azul-claro/50 cursor pointer rounded-md border-2 px-10 border-black">
                         Salvar
                     </Button>
                 </DialogFooter>
             </DialogContent>
-            </Dialog>
+            </Dialog>)
+}
         </>
     )
 
--- src/components/GestaoProjeto/cardProjeto.tsx
+++ src/components/GestaoProjeto/cardProjeto.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   Card,
   CardAction,
@@ -8,30 +9,35 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Button } from "../ui/button"
-import { Kanban, Pencil, RefreshCcw, X } from "lucide-react";
+import { Kanban, Pencil, RefreshCcw, X, Trash2 } from "lucide-react";
+import { RemoverProjeto } from "./removerProjeto";
+import { EditarProjeto } from "./editarProjeto";
+import { Projeto } from "@/core/interface/Projeto";
 
 interface CardProjetoProps {
-    nome: string;
-    descricao: string;
+    projeto: Projeto;
+    onRemove: (id: string) => void;
+    onEdit: (projeto: Projeto) => void;
 }
 
-export const CardProjeto = ({ nome, descricao }: CardProjetoProps) => {
+export const CardProjeto = ({ projeto, onRemove, onEdit }: CardProjetoProps) => {
+
     return (
         <Card className="w-72 bg-azul-escuro text-white rounded-md gap-2 pb-2">
             <CardHeader>
-                <CardTitle>{nome}</CardTitle>
+                <CardTitle>{projeto.nome}</CardTitle>
             </CardHeader>
             <CardContent>
                 <CardDescription className="text-white h-16 ">
-                    <p className="line-clamp-3"> {descricao}</p>
+                    <p className="line-clamp-3"> {projeto.descricao}</p>
                 </CardDescription>
             </CardContent>
             <CardFooter className="p-0 w-full flex items-end">
                 <CardAction className="flex w-full p-0 gap-2 justify-end px-2">
                         <Button className="bg-azul-claro hover:bg-transparent hover:cursor-pointer"> <Kanban /> </Button>
                         <Button className="bg-azul-claro hover:bg-transparent hover:cursor-pointer"> <RefreshCcw /> </Button>
-                        <Button className="bg-azul-claro hover:bg-transparent hover:cursor-pointer"> <Pencil /> </Button>
-                        <Button className="bg-azul-claro hover:bg-red-500 hover:cursor-pointer"> <X /> </Button>
+                        <EditarProjeto projeto={projeto} onEdit={onEdit} />
+                        <RemoverProjeto  projeto={projeto} onRemove={onRemove} />
                 </CardAction>
             </CardFooter>
         </Card>
