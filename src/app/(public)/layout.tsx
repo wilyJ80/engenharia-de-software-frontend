cde5520e69ec296486d64c96cac20137ffdaec44
Rafael <rafaelnargolo@outlook.com>
2025-10-29 14:07:06 -0300

    Atualizando

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -9,19 +9,19 @@ export default function RootLayout({
 }>) {
   return (
    <>
-        <div
-          className="w-[400px] flex flex-col items-center"
-          style={{
-            boxShadow: "2px 0 3px rgba(0, 0, 0, .4)"
-          }}
-        >
-          <img src="/logo.png" className="w-[65%]" alt="Logo" />
-          <Menu />
-        </div>
+    <div
+      className="w-[400px] flex flex-col items-center"
+      style={{
+        boxShadow: "2px 0 3px rgba(0, 0, 0, .4)"
+      }}
+    >
+      <img src="/logo.png" className="w-[65%]" alt="Logo" />
+      <Menu />
+    </div>
 
-        <div className="p-4 w-full">
-          {children}
-        </div>
+    <div className="p-4 w-full">
+      {children}
+    </div>
    </>
   );
 }
