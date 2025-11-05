3b4f4dcadd69ed981677b3b1f334ca765b0bfee3
wilyJ80 <abjurandam@gmail.com>
2025-11-05 08:26:44 -0300

    chore: output export

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  output: "standalone",
+	/* config options here */
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+	output: "export",
 };
 
 export default nextConfig;
