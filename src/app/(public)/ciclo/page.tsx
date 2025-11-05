6693e62ef6505ac2e3d4ce4c8180f88c1b68b4af
wilyJ80 <abjurandam@gmail.com>
2025-11-05 08:38:03 -0300

    fix: add client directive before export const edge

--- src/app/(public)/ciclo/page.tsx
+++ src/app/(public)/ciclo/page.tsx
@@ -1,6 +1,6 @@
-export const runtime = 'edge';
-
 "use client";
+
+export const runtime = 'edge';
 
 import { useState, useEffect } from "react";
 import CicloDialog from "@/components/Ciclos/CicloDialog";
